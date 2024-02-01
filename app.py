a4045981b64425be6760209be2a2715d48f2fff3
michael <michaeldwohlers@gmail.com>
2024-02-01 23:47:29 +0100

    Redis and offLoad integration

--- app.py
+++ app.py
@@ -1,27 +1,15 @@
-#app.py
+# app.py
 from flask import Flask
 from dotenv import load_dotenv
 import os
 from flask_socketio import SocketIO
 
+socketio = SocketIO()
+
 def create_app():
     load_dotenv()
-
     app = Flask(__name__)
     app.config['SECRET_KEY'] = os.environ.get('MY_FLASK_SECRET_KEY')
-
-    # Initialize Flask-SocketIO
-    # If you're strictly on the same domain and protocol, cors_allowed_origins can be set to '*'
-    # This is generally safe for same-origin deployments but be cautious about using '*' in a production environment
-    socketio = SocketIO(app, cors_allowed_origins="*")
-
-    # Import and configure routes
-    from routes import configure_routes
-    configure_routes(app)
+    socketio.init_app(app, cors_allowed_origins="*")
 
     return app, socketio
-
-if __name__ == "__main__":
-    app, socketio = create_app()
-    # Note: When running on Heroku, you might not need to specify host and port, Heroku sets it for you
-    socketio.run(app, debug=True)
--- run.py
+++ run.py
@@ -1,6 +1,11 @@
-#run.py
+# run.py or the bottom of app.py, if you don't use run.py
 from app import create_app, socketio
-app = create_app()
 
-if __name__ == '__main__':
-    socketio.run(app)
+app, socketio = create_app()
+
+# Ensure routes are imported after the app and socketio instances are created
+from routes import configure_routes
+configure_routes(app)
+
+if __name__ == "__main__":
+    socketio.run(app, debug=True)
