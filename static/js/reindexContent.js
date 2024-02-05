17227bb267787d9f6513aa14b31434d26a0d237c
michael <michaeldwohlers@gmail.com>
2024-02-05 23:53:48 +0100

    one-off integration

--- static/js/reindexContent.js
+++ static/js/reindexContent.js
@@ -559,7 +559,7 @@ $(document).ready(function() {
 
     socket.on('geojson_data', function(data) {
         console.log('Received GeoJSON data:', data);
-        console.log('Type of received data:', data.constructor.name);
+        console.log('Actual ArrayBuffer:', data.data); // If `data` is an object containing the ArrayBuffer
     
         // Check if data is an instance of ArrayBuffer
         if (data instanceof ArrayBuffer) {
